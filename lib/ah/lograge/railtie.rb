ec37725402addd683410a7dc293593cc3bc119cc
Przemyslaw Wroblewski <przemyslaw.wroblewski@gmail.com>
2017-06-14 08:22:26 +0200

    fixed interpretation of DISABLE_LOGRAGE env

--- lib/ah/lograge/railtie.rb
+++ lib/ah/lograge/railtie.rb
@@ -2,7 +2,13 @@ module Ah
   module Lograge
     class Railtie < ::Rails::Railtie
       initializer 'ah-lograge.setup_logging' do |app|
-        lograge_disabled = ENV['DISABLE_LOGRAGE'] || false
+        lograge_disabled = if Rails.version >= '4.2'
+          ActiveRecord::Type::Boolean.new.type_cast_from_database(ENV['DISABLE_LOGRAGE']) || false
+        elsif Rails.version >= '5.0'
+          ActiveRecord::Type::Boolean.new.cast(ENV['DISABLE_LOGRAGE']) || false
+        else
+          [true, 1, "1", "y", "true", "on", "yes"].include?(ENV['DISABLE_LOGRAGE'].to_s.downcase) || false
+        end
         app.config.lograge.enabled = !lograge_disabled
         app.config.lograge.formatter = ::Lograge::Formatters::Logstash.new
         app.config.lograge.custom_options = lambda do |event|
