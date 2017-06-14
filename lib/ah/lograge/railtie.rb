5bb467afad5f94fdd84ffac96c3c13b02844d32c
Przemyslaw Wroblewski <przemyslaw.wroblewski@gmail.com>
2017-06-14 08:25:28 +0200

    apply proper Rails.version comparison order

--- lib/ah/lograge/railtie.rb
+++ lib/ah/lograge/railtie.rb
@@ -2,10 +2,10 @@ module Ah
   module Lograge
     class Railtie < ::Rails::Railtie
       initializer 'ah-lograge.setup_logging' do |app|
-        lograge_disabled = if Rails.version >= '4.2'
-          ActiveRecord::Type::Boolean.new.type_cast_from_database(ENV['DISABLE_LOGRAGE']) || false
-        elsif Rails.version >= '5.0'
+        lograge_disabled = if Rails.version >= '5.0'
           ActiveRecord::Type::Boolean.new.cast(ENV['DISABLE_LOGRAGE']) || false
+        elsif Rails.version >= '4.2'
+          ActiveRecord::Type::Boolean.new.type_cast_from_database(ENV['DISABLE_LOGRAGE']) || false
         else
           [true, 1, "1", "y", "true", "on", "yes"].include?(ENV['DISABLE_LOGRAGE'].to_s.downcase) || false
         end
