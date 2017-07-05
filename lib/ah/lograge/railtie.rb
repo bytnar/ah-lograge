82ce767ba5b0519da724cdea688edc66359c2a0f
jakub-tokaj-ah <jakub.tokaj@airhelp.com>
2017-07-05 14:28:09 +0200

    Fixed lograge_disabled options.

--- lib/ah/lograge/railtie.rb
+++ lib/ah/lograge/railtie.rb
@@ -2,7 +2,7 @@ module Ah
   module Lograge
     class Railtie < ::Rails::Railtie
       initializer 'ah-lograge.setup_logging' do |app|
-        lograge_disabled = [ "no", "false", "0", "n", "off" ].include?(ENV['DISABLE_LOGRAGE'].to_s.downcase) || false
+        lograge_disabled = [ "yes", "true", "1", "y", "on" ].include?(ENV['DISABLE_LOGRAGE'].to_s.downcase) || false
         app.config.lograge.enabled = !lograge_disabled
         app.config.lograge.formatter = ::Lograge::Formatters::Logstash.new
         app.config.lograge.custom_options = lambda do |event|
